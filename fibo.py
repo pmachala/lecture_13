8575b31506d10503da9b8e886fc5d25bb603251f
Patrik Machala <patrikmachala11@gmail.com>
2021-05-03 10:46:17 +0200

    message

--- fibo.py
+++ fibo.py
@@ -1,6 +1,23 @@
+def recursive_nth_fibo(n):
+    if n ==0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
 
 def main():
-    pass
+    n = int(input("Zadejte pocet prvku n ="))
+    n_th = recursive_nth_fibo(n)
+
+    fib_seq = []
+    for num in range(n + 1):
+        fib_seq.append(recursive_nth_fibo(num))
+
+    print(fib_seq)
+
 
 
 if __name__ == '__main__':
